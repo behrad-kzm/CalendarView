3fd7a0867a4021b317847617bc55e178b2dbef4d
behrad kazemi <behradkzm@gmail.com>
2022-08-10 19:55:54 +0430

    add dependency to package target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "CalendarView",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CalendarView",
             targets: ["CalendarView"]),
@@ -20,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CalendarView",
-            dependencies: []),
+            dependencies: ["SwiftMoment"]),
         .testTarget(
             name: "CalendarViewTests",
             dependencies: ["CalendarView"]),
